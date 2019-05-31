09ebfc9657ce21e9672f35441333566ba5772e2b
assane savadogo <assane.savadogo81@gmail.com>
2019-05-31 04:50:55 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -18,8 +18,9 @@ end
 def while_iterator(int)
   int = 0 
   while int < 7
+  int += 1
   puts "Welcome to Flatiron School's Web Development Course!"
-  
+ end 
 end
 
 def until_iterator(number_of_times)
